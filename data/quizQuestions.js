22918e196b942dbb1fbce111006d343aa8747bfb
Alexa Mullin <alemulli@gmail.com>
2023-04-17 17:01:32 -0400

    added a file we can store the quiz questions in data/quizQuestions.js

---
+++ data/quizQuestions.js
@@ -0,0 +1,23 @@
+htmlQuestions = [
+  { question: "", 
+  correctAnswer: "", 
+  incorrectAnswers: ["", "", ""] },
+];
+
+cssQuestions = [
+    { question: "", 
+    correctAnswer: "", 
+    incorrectAnswers: ["", "", ""] },
+];
+
+javascriptQuestions = [
+    { question: "", 
+    correctAnswer: "", 
+    incorrectAnswers: ["", "", ""] },
+];
+
+module.export = {
+    htmlQuestions,
+    cssQuestions,
+    javascriptQuestions
+}
