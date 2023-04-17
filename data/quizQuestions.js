051a8cb9110ef7b8ad432a3ee6ab4d9ebbbfe9de
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-04-17 17:26:42 -0400

    added 3 HTML questions

--- data/quizQuestions.js
+++ data/quizQuestions.js
@@ -1,23 +1,32 @@
 htmlQuestions = [
-  { question: "", 
-  correctAnswer: "", 
-  incorrectAnswers: ["", "", ""] },
+  {
+    question: "What does HTML stand for?",
+    correctAnswer: "Hypertext Markup Language",
+    incorrectAnswers: [
+      "Hypertext Makeup Language",
+      "HTML Markup Language",
+      "Hyper Markup Language",
+    ],
+    question: "What is the purpose of the <!DOCTYPE> declaration in HTML?",
+    correctAnswer: "specifies the document type and version of HTML being used",
+    incorrectAnswers: [
+      "The <!DOCTYPE> declaration in HTML specifies the color scheme of the web page",
+      "The <!DOCTYPE> declaration in HTML specifies the programming language used to create the web page",
+      "The <!DOCTYPE> declaration in HTML is not necessary and can be omitted",
+    ],
+  },
 ];
 
 cssQuestions = [
-    { question: "", 
-    correctAnswer: "", 
-    incorrectAnswers: ["", "", ""] },
+  { question: "", correctAnswer: "", incorrectAnswers: ["", "", ""] },
 ];
 
 javascriptQuestions = [
-    { question: "", 
-    correctAnswer: "", 
-    incorrectAnswers: ["", "", ""] },
+  { question: "", correctAnswer: "", incorrectAnswers: ["", "", ""] },
 ];
 
 module.export = {
-    htmlQuestions,
-    cssQuestions,
-    javascriptQuestions
-}
+  htmlQuestions,
+  cssQuestions,
+  javascriptQuestions,
+};
