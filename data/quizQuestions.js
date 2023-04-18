9902dece4951d7c366b28aaf98333a45597fa333
Alexa Mullin <alemulli@gmail.com>
2023-04-17 22:10:12 -0400

    editted the format of the questions in the question bank so that it will be easier to randomize the order of the answers in the display

--- data/quizQuestions.js
+++ data/quizQuestions.js
@@ -2,27 +2,29 @@ htmlQuestions = [
   {
     question: "What does HTML stand for?",
     correctAnswer: "Hypertext Markup Language",
-    incorrectAnswers: [
+    allAnswers: [
       "Hypertext Makeup Language",
       "HTML Markup Language",
+      "Hypertext Markup Language",
       "Hyper Markup Language",
     ],
     question: "What is the purpose of the <!DOCTYPE> declaration in HTML?",
-    correctAnswer: "specifies the document type and version of HTML being used",
-    incorrectAnswers: [
+    correctAnswer: "The <!DOCTYPE> declaration in HTML specifies the document type and version of HTML being used",
+    allAnswers: [
       "The <!DOCTYPE> declaration in HTML specifies the color scheme of the web page",
       "The <!DOCTYPE> declaration in HTML specifies the programming language used to create the web page",
       "The <!DOCTYPE> declaration in HTML is not necessary and can be omitted",
+      "The <!DOCTYPE> declaration in HTML specifies the document type and version of HTML being used"
     ],
   },
 ];
 
 cssQuestions = [
-  { question: "", correctAnswer: "", incorrectAnswers: ["", "", ""] },
+  { question: "", correctAnswer: "", allAnswers: ["", "", "", ""] },
 ];
 
 javascriptQuestions = [
-  { question: "", correctAnswer: "", incorrectAnswers: ["", "", ""] },
+  { question: "", correctAnswer: "", allAnswers: ["", "", "", ""] },
 ];
 
 module.export = {
