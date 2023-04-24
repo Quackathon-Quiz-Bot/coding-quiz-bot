fdaba603022fd1c2cfbd2dd14c269eb9d583d61e
Jessy <japarker0421@gmail.com>
2023-04-24 14:52:55 -0600

    bot is correctly identifying correct and incorrect answers

--- data/quizQuestions.js
+++ data/quizQuestions.js
@@ -30,185 +30,185 @@ htmlQuestions = [
 ];
 
 cssQuestions = [
-  { 
-    question: "What does CSS stand for?", 
-    correctAnswer: "Cascading Style Sheets", 
+  {
+    question: "What does CSS stand for?",
+    correctAnswer: "Cascading Style Sheets",
     allAnswers: [
-      "Creating Simple Styles", 
-      "Cascading Simple Styles", 
+      "Creating Simple Styles",
+      "Cascading Simple Styles",
       "Colorful Style Sheets"
-    ] 
+    ]
   },
-  { 
-    question: "How would you link your stylesheet to your HTML (in the HTML file)?", 
-    correctAnswer: "<link rel='stylesheet' href='style.css'>", 
+  {
+    question: "How would you link your stylesheet to your HTML (in the HTML file)?",
+    correctAnswer: "<link rel='stylesheet' href='style.css'>",
     allAnswers: [
-      "<link rel='stylesheet'>", 
-      "<link rel='style.css'>", 
+      "<link rel='stylesheet'>",
+      "<link rel='style.css'>",
       "<link rel='style.css' href='stylesheet'>"
-    ] 
+    ]
   },
-  { 
-    question: "Where in the HTML file would you link your stylesheet(s)?", 
-    correctAnswer: "In the <head> section", 
+  {
+    question: "Where in the HTML file would you link your stylesheet(s)?",
+    correctAnswer: "In the <head> section",
     allAnswers: [
-      "In the <body> section", 
-      "Outside of the <html> section", 
+      "In the <body> section",
+      "Outside of the <html> section",
       "Inside the <html> section, but outside of <head> and <body>"
-    ] 
+    ]
   },
-  { 
-    question: "What is the correct format for inserting comments in a CSS file?", 
-    correctAnswer: "/* comment */", 
+  {
+    question: "What is the correct format for inserting comments in a CSS file?",
+    correctAnswer: "/* comment */",
     allAnswers: [
       "// comment //", //javascript comment
       "<!-- comment -->", //html comment
       "# comment" //python comment
-    ] 
+    ]
   },
-  { 
-    question: "Which of these is an incorrect way to format the color white in CSS?", 
-    correctAnswer: "None of these", 
+  {
+    question: "Which of these is an incorrect way to format the color white in CSS?",
+    correctAnswer: "None of these",
     allAnswers: [
       "#FFFFFF", //hex
       "white", //one of CSS's default named colors
       "rgb(255, 255, 255)" //rgb
-    ] 
+    ]
   },
-  { 
-    question: "How would you select an element with a specific id in CSS?", 
-    correctAnswer: "#id", 
+  {
+    question: "How would you select an element with a specific id in CSS?",
+    correctAnswer: "#id",
     allAnswers: [
-      ".id", 
-      "id", 
+      ".id",
+      "id",
       "*id"
-    ] 
+    ]
   },
-  { 
-    question: "How would you select elements with a specific class in CSS?", 
-    correctAnswer: ".class", 
+  {
+    question: "How would you select elements with a specific class in CSS?",
+    correctAnswer: ".class",
     allAnswers: [
-      "#class", 
-      "class", 
+      "#class",
+      "class",
       "*class"
-    ] 
+    ]
   },
-  { 
-    question: "Which of the following would be the correct syntax for giving all h1 elements a black font-color?", 
-    correctAnswer: "h1 {color: black}", 
+  {
+    question: "Which of the following would be the correct syntax for giving all h1 elements a black font-color?",
+    correctAnswer: "h1 {color: black}",
     allAnswers: [
-      "h1: color = black", 
-      "*h1 {color = black}", 
+      "h1: color = black",
+      "*h1 {color = black}",
       "{h1: black}"
-    ] 
+    ]
   },
-  { 
-    question: "Which of the following would be the correct syntax for selecting all p elements inside of a div with the id 'id'?", 
-    correctAnswer: "#id p", 
+  {
+    question: "Which of the following would be the correct syntax for selecting all p elements inside of a div with the id 'id'?",
+    correctAnswer: "#id p",
     allAnswers: [
-      "#id.p", 
-      "#id + p", 
+      "#id.p",
+      "#id + p",
       "#id, p"
-    ] 
+    ]
   },
-  { 
-    question: "Which of the following would be the correct syntax for giving all <aside> elements and anything with the class name 'class' a padding of 5 pixels?", 
-    correctAnswer: "aside, .class {padding: 5px}", 
+  {
+    question: "Which of the following would be the correct syntax for giving all <aside> elements and anything with the class name 'class' a padding of 5 pixels?",
+    correctAnswer: "aside, .class {padding: 5px}",
     allAnswers: [
-      "aside, .class {padding: 5p", 
-      "aside .class {padding: 5px}", 
+      "aside, .class {padding: 5p",
+      "aside .class {padding: 5px}",
       "{aside, .class: padding = 5px}"
-    ] 
+    ]
   },
-  { 
-    question: "What is the default value of the position property if not changed manually?", 
-    correctAnswer: "static", 
+  {
+    question: "What is the default value of the position property if not changed manually?",
+    correctAnswer: "static",
     allAnswers: [
-      "relative", 
-      "absolute", 
+      "relative",
+      "absolute",
       "fixed"
-    ] 
+    ]
   },
-  { 
-    question: "Which of the following is a pseudo-class?", 
-    correctAnswer: ":hover", 
+  {
+    question: "Which of the following is a pseudo-class?",
+    correctAnswer: ":hover",
     allAnswers: [
-      "::after", 
-      "::before", 
+      "::after",
+      "::before",
       "::firstletter"
-    ] 
+    ]
   },
-  { 
-    question: "Which of the following is a pseudo-element?", 
-    correctAnswer: "::first-letter", 
+  {
+    question: "Which of the following is a pseudo-element?",
+    correctAnswer: "::first-letter",
     allAnswers: [
-      ":hover", 
-      ":active", 
+      ":hover",
+      ":active",
       ":nth-child"
-    ] 
+    ]
   },
-  { 
-    question: "How would you select for elements containing a specific attribute, such as divs containing a language attribute - <div lang='en-us'>?", 
-    correctAnswer: "div[lang]", 
+  {
+    question: "How would you select for elements containing a specific attribute, such as divs containing a language attribute - <div lang='en-us'>?",
+    correctAnswer: "div[lang]",
     allAnswers: [
-      "div[lang|='en']", 
-      "div:not([lang])", 
+      "div[lang|='en']",
+      "div:not([lang])",
       "div[lang~='en-us']"
-    ] 
+    ]
   },
-  { 
-    question: "How would you select for elements containing a specific attribute and value, such as divs containing a language attribute and the US english property - <div lang='en-us'>?", 
-    correctAnswer: "div[lang~='en-us']", 
+  {
+    question: "How would you select for elements containing a specific attribute and value, such as divs containing a language attribute and the US english property - <div lang='en-us'>?",
+    correctAnswer: "div[lang~='en-us']",
     allAnswers: [
-      "div[lang|='en']", 
-      "div:not([lang])", 
+      "div[lang|='en']",
+      "div:not([lang])",
       "div[lang]"
-    ] 
+    ]
   },
-  { 
-    question: "How would you select for elements that are the direct children of other elements, such as <li> that are the direct children of <ul>?", 
-    correctAnswer: "ul > li", 
+  {
+    question: "How would you select for elements that are the direct children of other elements, such as <li> that are the direct children of <ul>?",
+    correctAnswer: "ul > li",
     allAnswers: [
-      "ul li", 
-      "ul + li", 
+      "ul li",
+      "ul + li",
       "ul[li]"
-    ] 
+    ]
   },
-  { 
-    question: "Which At-rule allows you to build custom CSS animations?", 
-    correctAnswer: "@keyframes", 
+  {
+    question: "Which At-rule allows you to build custom CSS animations?",
+    correctAnswer: "@keyframes",
     allAnswers: [
-      "@media", 
-      "@animations", 
+      "@media",
+      "@animations",
       "@import"
-    ] 
+    ]
   },
-  { 
-    question: "When applying specific border widths to all four-sides of a div using shorthand properties, in which order would the sides be listed?", 
-    correctAnswer: "div {border-width: top right bottom left}", 
+  {
+    question: "When applying specific border widths to all four-sides of a div using shorthand properties, in which order would the sides be listed?",
+    correctAnswer: "div {border-width: top right bottom left}",
     allAnswers: [
-      "div {border-width: top left bottom right}", 
-      "div {border-width: top bottom left right}", 
+      "div {border-width: top left bottom right}",
+      "div {border-width: top bottom left right}",
       "div {border-width: left right top bottom}"
-    ] 
+    ]
   },
-  { 
-    question: "Which CSS selector has the highest specificity?", 
-    correctAnswer: "#id", 
+  {
+    question: "Which CSS selector has the highest specificity?",
+    correctAnswer: "#id",
     allAnswers: [
-      ".class", 
-      "type", 
+      ".class",
+      "type",
       "* universal"
-    ] 
+    ]
   },
-  { 
-    question: "Which CSS selector has the lowest specificity?", 
-    correctAnswer: "* universal", 
+  {
+    question: "Which CSS selector has the lowest specificity?",
+    correctAnswer: "* universal",
     allAnswers: [
-      ".class", 
-      "type", 
+      ".class",
+      "type",
       "#id"
-    ] 
+    ]
   },
 ];
 
@@ -248,13 +248,13 @@ javascriptQuestions = [
     "A browser extension for accessing websites",
     "A system for storing website data"
   ] },
-  { question: "What is the main difference between forEach and Map",
-  correctAnswer: "forEach iterates over an array and changes its elements, while Map creates a new array with the same length",
-  allAnswers: [
-    "forEach and Map both create a new array, but Map only works with numbers",
-    "There is no difference, they are interchangeable",
-    "Map iterates over an array and changes its elements, while forEach creates a new array with the same length"
-  ] },
+  // { question: "What is the main difference between forEach and Map",
+  // correctAnswer: "forEach iterates over an array and changes its elements, Map creates a new array with the same length",
+  // allAnswers: [
+  //   "forEach and Map both create a new array, but Map only works with numbers",
+  //   "There is no difference, they are interchangeable",
+  //   "Map iterates over an array and changes its elements, forEach creates a new array with the same length"
+  // ] },
   {
     question: "What is the correct way to convert a string to uppercase in JavaScript?",
     correctAnswer: "myString.toUpperCase()",
